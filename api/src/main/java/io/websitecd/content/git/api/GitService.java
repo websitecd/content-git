34bcc9a69756f1c1e28127aada5d02be203c411e
Libor Krzyzanek <libor@krzyzanek.com>
2021-01-19 20:15:58 +0100

    api - implementation

---
+++ api/src/main/java/io/websitecd/content/git/api/GitService.java
@@ -0,0 +1,46 @@
+package io.websitecd.content.git.api;
+
+import io.quarkus.runtime.StartupEvent;
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.errors.GitAPIException;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+import org.jboss.logging.Logger;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.event.Observes;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Arrays;
+import java.util.List;
+
+@ApplicationScoped
+public class GitService {
+
+    Logger log = Logger.getLogger(GitService.class);
+
+    @ConfigProperty(name = "app.data.dir")
+    String dataDir;
+
+    void onStart(@Observes StartupEvent ev) {
+        log.infof("content-git-api started. APP_DATA_DIR=%s", dataDir);
+    }
+
+    public List<String> list() {
+        File file = new File(dataDir);
+        String[] directories = file.list((current, name) -> new File(current, name).isDirectory());
+
+        return Arrays.asList(directories);
+    }
+
+    public void updateGit(String dir) throws FileNotFoundException, GitAPIException {
+        log.infof("Update git %s", dir);
+        File baseDir = new File(dataDir);
+        File gitDir = new File(baseDir.getAbsolutePath() + "/" + dir);
+        if (!gitDir.exists()) {
+            throw new FileNotFoundException("dir " + gitDir.getAbsolutePath() + " does not exist");
+        }
+
+        Git git = Git.init().setDirectory(gitDir).call();
+        git.pull();
+    }
+}
---
+++ api/src/main/java/io/websitecd/content/git/api/rest/GitApiResource.java
@@ -0,0 +1,33 @@
+package io.websitecd.content.git.api.rest;
+
+import io.websitecd.content.git.api.GitService;
+import org.eclipse.jgit.api.errors.GitAPIException;
+
+import javax.inject.Inject;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import java.io.FileNotFoundException;
+import java.util.List;
+
+@Path("/api")
+public class GitApiResource {
+
+    @Inject
+    GitService gitService;
+
+    @GET
+    @Path("list")
+    public List<String> list() {
+        return gitService.list();
+    }
+
+    @GET
+    @Path("update/{dir}")
+    public String list(@PathParam("dir") String dir) throws FileNotFoundException, GitAPIException {
+        gitService.updateGit(dir);
+        return "DONE";
+    }
+
+
+}
