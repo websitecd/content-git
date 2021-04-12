12e3cb5f62d37498941c2f3982ece8c73f409fd8
Libor Krzyzanek <libor@krzyzanek.com>
2021-04-12 13:04:48 +0200

    added simple health check test

---
+++ api/src/test/java/io/spaship/content/git/api/rest/HealthCheckResourceTest.java
@@ -0,0 +1,34 @@
+package io.spaship.content.git.api.rest;
+
+import io.quarkus.test.common.http.TestHTTPEndpoint;
+import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.is;
+
+@QuarkusTest
+@TestHTTPEndpoint(HealthCheckResource.class)
+class HealthCheckResourceTest {
+
+    @Test
+    void live() {
+        given()
+                .when().get("/live")
+                .then()
+                .log().ifValidationFails()
+                .statusCode(200)
+                .body(is("live"));
+    }
+
+    @Test
+    void ready() {
+        given()
+                .when().get("/ready")
+                .then()
+                .log().ifValidationFails()
+                .statusCode(200)
+                .body(is("ready"));
+    }
+
+}
